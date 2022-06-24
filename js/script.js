9f3e08536dd9d9a7892d118e68f25f6ec96ead1e
PammiKumari1234 <pammisao1234@gmail.com>
2022-06-24 19:52:18 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// typing animation effect
+
+var typed= new Typed(".typing",{
+    strings:[" ", "Web Designer","Graphic Designer","Web Developer"],
+    tyepeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
