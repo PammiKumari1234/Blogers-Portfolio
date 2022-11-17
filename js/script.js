a3535399835b2850500445401078ac8a64acbe9d
Pammi Kumari <75567737+PammiKumari1234@users.noreply.github.com>
2022-11-17 20:48:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // typing animation effect
 
 var typed= new Typed(".typing",{
-    strings:[" ", "Web Designer","Graphic Designer","Web Developer"],
-    tyepeSpeed:100,
-    BackSpeed:60,
+    strings:["", "Writer","Photographer","Speaker"],
+    tyepeSpeed:10000,
+    BackSpeed:600,
     loop:true
 })
